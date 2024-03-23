7eadeea5e729e569cf881f26bffe7a67741f1448
ken <kencfd@users.noreply.github.com>
2024-03-23 15:03:07 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,4 @@
 var modifiedHeaders = $request.headers;
-
 modifiedHeaders['X-Forwarded-For'] = '117.136.39.0';
 modifiedHeaders['X-Real-IP'] = '117.136.39.0';
-
-
 $done({headers : modifiedHeaders});
